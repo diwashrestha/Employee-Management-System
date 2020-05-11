6e071b658481ef05677a3067783351fda781b091
diwashrestha <diwash712@gmail.com>
2020-05-11 23:48:25 +0545

    global exception handling

--- EmployeeManagement/Startup.cs
+++ EmployeeManagement/Startup.cs
@@ -35,6 +35,11 @@ namespace EmployeeManagement
 
                 app.UseDeveloperExceptionPage();
             }
+            else
+            {
+                app.UseExceptionHandler("/Error");
+                app.UseStatusCodePagesWithReExecute("/Error/{0}");
+            }
 
 
             app.UseStaticFiles();
